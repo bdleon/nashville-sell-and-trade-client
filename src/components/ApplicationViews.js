fb873a7f3f01edf7ebee09cca89eb02eadd47a65
Brandon De Leon <brandondeleon.615@gmail.com>
2021-12-07 09:03:39 -0600

    first commit

---
+++ src/components/ApplicationViews.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const ApplicationViews = () =>{
+    return(
+        <>
+        <p>hi</p>
+        <h1>hi</h1>
+        </>
+    )
+}
---
+++ src/components/NashSellAndTrade.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { ApplicationViews } from "./ApplicationViews";
+
+
+export const NashSellAndTrade = ()=>{
+    
+    
+    
+    return(
+        <>
+        
+        <ApplicationViews />
+        </>
+    )
+
+}
+
