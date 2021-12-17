baea4f22e4e5ec685ae2a1a2382a2d8e480b9549
Brandon De Leon <brandondeleon.615@gmail.com>
2021-12-17 07:07:19 -0600

    added recieved messages

--- src/components/message/MessageList.js
+++ src/components/message/MessageList.js
@@ -1,8 +1,48 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { getMessages } from "./MessageManager";
+import "../message/Message.css"
 
-export const MessageList = () =>{
-    return(
+export const MessageList = () => {
+
+    const [messages, setMessages] = useState([])
+
+    useEffect(() => {
+        getMessages().then(data => setMessages(data))
+
+    }, [])
+
+
+
+
+
+    return (
         <>
+            <Container>
+                <Row>
+                    <Col sm={3}>
+                        <h2>Messages</h2>
+
+                    </Col>
+                    <Col sm={9}>
+                        {
+                            messages.length !== 0 ? <>
+                                {messages?.map(message => {
+                                    return <div key={`message--${message.id}`} className="message-container">
+                                        <p>sender: {message.sender.user.username}</p>
+                                        <p>product: {message.product.title}</p>
+                                        <p>message: {message.message}</p>
+                                    </div>
+                                })}
+                            </> : <><p>no messages</p></>
+                        }
+
+                    </Col>
+                </Row>
+
+            </Container>
         </>
     )
 }
