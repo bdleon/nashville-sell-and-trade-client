740926fc6a9f0f6d1ac98d9830e968da30511802
Brandon De Leon <brandondeleon.615@gmail.com>
2022-01-04 18:58:29 -0600

    added bootstrap nav

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,6 +1,9 @@
 import React from "react"
-import { Link,useHistory } from "react-router-dom"
+import { Link, useHistory } from "react-router-dom"
 import "./NavBar.css"
+import Navbar from 'react-bootstrap/Navbar'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
 
 export const NavBar = () => {
     const history = useHistory()
@@ -39,6 +42,38 @@ export const NavBar = () => {
                         </>
                 }
             </ul>
+            <Navbar bg="dark" variant="dark">
+                <Container>
+                    <Navbar.Brand href="/">Nashville sell and trade</Navbar.Brand>
+                    <Nav className="me-auto">
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/product/my_products">My Products</Nav.Link>
+                        <Nav.Link href="/form">Add product</Nav.Link>
+                        <Nav.Link href="/product/messages">My Messages</Nav.Link>
+                        {
+                    (localStorage.getItem("nst_token") !== null) ?
+                        <li className="nav-item">
+                            <button className="nav-link fakeLink"
+                                onClick={() => {
+                                    localStorage.removeItem("nst_token")
+                                    history.push({ pathname: "/" })
+                                }}
+                            >Logout</button>
+                        </li> :
+                        <>
+                            <li className="nav-item">
+                                
+                                <Nav.Link href="/login">Login</Nav.Link>
+                            </li>
+                            <li className="nav-item">
+                                
+                                <Nav.Link href="/register">Register</Nav.Link>
+                            </li>
+                        </>
+                }
+                    </Nav>
+                </Container>
+            </Navbar>
         </>
     )
 }
