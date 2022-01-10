e115416b362fc6a5a4963ed304d458dd419137dd
Brandon De Leon <brandondeleon.615@gmail.com>
2022-01-10 15:09:34 -0600

    added logo to navbar

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -10,39 +10,44 @@ export const NavBar = () => {
     const history = useHistory()
     return (
         <>
-            
+
             <Navbar bg="light" variant="light" expand="lg" >
                 <Container>
-                    <Navbar.Brand href="/">Nashville sell and trade</Navbar.Brand>
+                    <Navbar.Brand href="/">
+                        <img
+                            src="./Nashville S-2-2.png"
+                            width="100"
+                            height="100" />
+                    </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="navbarScroll">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/product/my_products">My Products</Nav.Link>
-                        <Nav.Link href="/form">Add product</Nav.Link>
-                        <Nav.Link href="/product/messages">My Messages</Nav.Link>
-                        {
-                    (localStorage.getItem("nst_token") !== null) ?
-                        <li className="nav-item">
-                            <Button className="nav-link fakeLink"
-                                onClick={() => {
-                                    localStorage.removeItem("nst_token")
-                                    history.push({ pathname: "/" })
-                                }}
-                            ><span className="logout-button">Logout</span></Button>
-                        </li> :
-                        <>
-                            <li className="nav-item">
-                                
-                                <Nav.Link href="/login">Login</Nav.Link>
-                            </li>
-                            <li className="nav-item">
-                                
-                                <Nav.Link href="/register">Register</Nav.Link>
-                            </li>
-                        </>
-                }
-                    </Nav>
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/product/my_products">My Products</Nav.Link>
+                            <Nav.Link href="/form">Add product</Nav.Link>
+                            <Nav.Link href="/product/messages">My Messages</Nav.Link>
+                            {
+                                (localStorage.getItem("nst_token") !== null) ?
+                                    <li className="nav-item">
+                                        <Button className="nav-link fakeLink"
+                                            onClick={() => {
+                                                localStorage.removeItem("nst_token")
+                                                history.push({ pathname: "/" })
+                                            }}
+                                        ><span className="logout-button">Logout</span></Button>
+                                    </li> :
+                                    <>
+                                        <li className="nav-item">
+
+                                            <Nav.Link href="/login">Login</Nav.Link>
+                                        </li>
+                                        <li className="nav-item">
+
+                                            <Nav.Link href="/register">Register</Nav.Link>
+                                        </li>
+                                    </>
+                            }
+                        </Nav>
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
